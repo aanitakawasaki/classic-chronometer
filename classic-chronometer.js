280af95a5f87580929eb0b047e932afc36ee4715
Anita Yumi Kawasaki <aanitakawasaki@gmail.com>
2023-03-12 18:16:45 -0300

    bringing classic projects files to GitHub

---
+++ classic-chronometer.js
@@ -0,0 +1,9 @@
+let start = document.querySelector ('input.start');
+start.addEventListener ('click', startClicked);
+
+let chronometer = document.querySelector ('p.chronometer');
+//console.log (chronometer);
+
+function startClicked () {
+    console.log (`clicou`);
+}
